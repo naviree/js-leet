609e0d334d1613014198c40d2017ad482e49c8aa
Jeremy Jefferson <jeremyijefferson@gmail.com>
2023-12-06 15:16:05 -0800

    Add reduceNumber.js file

---
+++ js/reduceNumber.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+let num = 14;
+let number = 0;
+let steps = 0;
+let numberOfSteps = (num) => {
+    let steps = 0;
+    while (num > 0) {
+        if (num % 2 === 0) {
+            num /= 2;
+        } else {
+            num--;
+        }
+        steps++;
+    }
+    return num;
+};
+
+console.log(numberOfSteps(num));
