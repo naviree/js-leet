869aa7f443052f392b6013f5bd65d5b91dc9fc04
Jeremy Jefferson <jeremyijefferson@gmail.com>
2023-12-06 11:25:58 -0800

    finished

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -14,15 +14,18 @@ num = prompt("Please enter a number:")
 let totalArray = []
 const fizzBuzz = function(n) {
 
-    for(let i = 1; i <= num; i++){
-        totalArray.push(i.toString());
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 ===0) {
+            totalArray.push("FizzBuzz");
+        } else if (i % 3 === 0 ) {
+            totalArray.push("Fizz")
+        } else if (i % 5 === 0) {
+            totalArray.push("Buzz");
+        } else {
+            totalArray.push(i);
+        }
     }
-    console.log(totalArray)
+    console.log(totalArray);
 
-    if (num % 3 === 0) {
-
-
-    }
-};
-
+}
 fizzBuzz(num);
