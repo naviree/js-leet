6717e3aa420b5176394e5176b683d2935bef2353
Jeremy Jefferson <jeremyijefferson@gmail.com>
2023-12-07 11:42:46 -0800

    Fix initialization of ListNode and head variables

--- js/middleOfList.js
+++ js/middleOfList.js
@@ -2,7 +2,14 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-let head = [];
+let ListNode = [];
+let head = 5;
+let middleNumber = 0;
 let middleNode = function (head) {
-    
+	for (let i = 1; i <= head; i++) {
+		ListNode.push(i);
+	}
+	return ListNode;
 };
+
+console.log(middleNode(head));
