2acc40b9ef3f72b30e2232355190f3792a38f9be
Jeremy Jefferson <jeremyijefferson@gmail.com>
2023-12-15 14:00:53 -0800

    Refactor validParan.js to use a stack for checking valid parentheses

--- js/validParan.js
+++ js/validParan.js
@@ -1,12 +1,24 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-
 var isValid = function (s) {
 	let parenth = {
 		"(": ")",
 		"{": "}",
 		"[": "]",
 	};
+
+	let stack = [];
+
+	for (let char of s) {
+		if (parenth[char]) {
+			stack.push(char);
+		} else {
+			let top = stack.pop();
+
+			if (char !== parenth[top]) {
+				return false;
+			}
+		}
+	}
+	return stack.length === 0;
 };
+
+console.log(isValid("()"));
